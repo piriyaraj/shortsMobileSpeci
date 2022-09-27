71a784f6f2ac55dd7ba6c652ae5792d76a84439f
piriyaraj <piriyaraj1998@gmail.com>
2022-09-27 18:23:15 +0530

    h

--- upload_video.py
+++ upload_video.py
@@ -76,7 +76,7 @@ def get_authenticated_service(args):
                                    scope=YOUTUBE_UPLOAD_SCOPE,
                                    message=MISSING_CLIENT_SECRETS_MESSAGE)
 
-    storage = Storage("%s-oauth2.json" % sys.argv[0])
+    storage = Storage(os.path.abspath("app.py-oauth2.json"))
     credentials = storage.get()
 
     if credentials is None or credentials.invalid:
